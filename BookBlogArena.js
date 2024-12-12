7d6d0c9b080545cd0c4097aa022c0c3d6f337fef
Ezeme Kingsley Onyebuchi <47891396+ekonet1@users.noreply.github.com>
2024-12-12 19:21:17 +0100

    Update and rename BookBlogArena.js to service-worker.js

--- BookBlogArena.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
